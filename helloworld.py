d1e673de3424d6d65fe4179fb2147364a4a1aece
Yongguang <mayongguang86@gmail.com>
2019-06-04 20:35:38 -0400

    hello world

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+s = 'Hello World!'
+print(s)
+
+my_integer = 5
+my_floating_point = 26.2
+my_Boolean = True
+my_string = 'charactor'
+my_integer = 0xFF
+
+s = 'hello' + 'world'
+s = s * 4
+print(s)
