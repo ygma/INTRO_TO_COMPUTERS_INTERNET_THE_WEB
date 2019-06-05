25c2524f4bd54ca2069547102a1cdcd5749c5061
Yongguang <mayongguang86@gmail.com>
2019-06-05 18:39:08 -0400

    control structure

---
+++ Control_Structure.py
@@ -0,0 +1,21 @@
+n = 1
+sum = 0
+
+while(n <= 100):
+    if n % 2 == 0:
+        sum += n
+        
+    n += 1  
+        
+print(sum)
+    
+
+
+
+#n = 0
+#sum = 0
+#while n <= 100:
+#    sum += n
+#    n += 2
+#    
+#print(sum)
