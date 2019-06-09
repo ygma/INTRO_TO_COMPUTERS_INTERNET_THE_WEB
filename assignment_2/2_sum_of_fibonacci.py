e42dd29142118907290713079455ed28ba13100c
Yongguang <mayongguang86@gmail.com>
2019-06-08 22:45:30 -0400

    add ut for sum fibo

--- assignment_2/2_sum_of_fibonacci.py
+++ assignment_2/2_sum_of_fibonacci.py
@@ -1,4 +1,7 @@
+import unittest
+
 def cal_sum_fibo(n):
+    if n == 1: return 1
     a, b = 1, 1
     sum = a + b
     for i in range(3, n + 1):
@@ -7,5 +10,11 @@ def cal_sum_fibo(n):
         a, b = b,c
         
     return sum
-
-print(cal_sum_fibo(8))
+    
+class TestCase(unittest.TestCase):
+    def test_should_return_sum_of_fibo(self):
+        self.assertEqual(2, cal_sum_fibo(2))
+        self.assertEqual(54, cal_sum_fibo(8))
+        self.assertEqual(1, cal_sum_fibo(1))
+        
+if __name__ == '__main__': unittest.main()   
